f49353497851b76c1f595983ca0c0c428564fe74
hanggarahadianto <93454559+hanggarahadianto@users.noreply.github.com>
2022-05-05 04:59:16 +0700

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <div id="home" className="pt-36">
+        <div className="container">
+          <div className="flex flex-wrap">
+            <div className="w-full self-center px-4">
+              <h1>Hallo all 😊😊😊😊😊</h1>
+            </div>
+          </
